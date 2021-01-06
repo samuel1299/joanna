074122c626dd17f08653dc341090ed89cd89c7ed
91955 <91955@DESKTOP-JQKI8L0>
2021-01-06 22:57:21 +0530

    spring

---
+++ CodeJavaAppp/src/main/java/com/example/demo/AppController.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AppController {
+	
+	@Autowired
+    private UserRepository repo;
+	
+	@GetMapping("")
+	public String viewHomePage()  {
+        return "index";
+
+	}
+        @GetMapping("/register")
+        public String showRegistrationForm(Model model) {
+            model.addAttribute("user", new User());
+             
+            return "signup_form";
+        }
+
+        @PostMapping("/process_register")
+        public String processRegister(User user) {
+        	
+        	repo.save(user);
+        	
+        	return "register_success";
+        
+        }
+        
+}
+
---
+++ CodeJavaAppp/src/test/java/com/example/demo/UserRepositoryTests.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(true)
+public class UserRepositoryTests {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	@Test
+	public void testCreateUser() {
+		User user = new User();
+		    user.setEmail("samrocks12999@gmail.com");
+		    user.setPassword("1233");
+		    user.setFirstname("samuell");
+		    user.setLastname("samm");
+	
+		
+		    User savedUser = repo.save(user);
+		     
+		    User existUser = entityManager.find(User.class, savedUser.getId());
+	
+	
+		    assertThat(existUser.getEmail()).isEqualTo(user.getEmail());
+	
+
+}
+}
